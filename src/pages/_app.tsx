c46f9ce96048be646ce778b9c1caedaefa97f31a
louis holley <louisholley@me.com>
2022-10-16 10:55:08 +0100

    remove react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
-const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
